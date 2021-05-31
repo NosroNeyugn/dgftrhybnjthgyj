3bb4d022003d29bc666bc7b2c81b1d8453040dc2
NosroNeyugn <69816627+NosroNeyugn@users.noreply.github.com>
2021-05-31 16:30:52 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { StyleSheet, Text, View, image } from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import WriteStoryScreen from './screens/WriteStoryScreen';
+import ReadStoryScreen from './screens/ReadStoryScreen'
